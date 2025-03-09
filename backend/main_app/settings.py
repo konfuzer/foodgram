5a361a13a432f527cb43fa7747892e12d1b6b4c4
Ivan <ivan.artemov.04@gmail.com>
2025-03-09 12:13:36 +0300

    created user accounts app

--- backend/main_app/settings.py
+++ backend/main_app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'recipes.apps.RecipesConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.UserModel'
+
