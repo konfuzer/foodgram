79e16d0577b3a73c82c1a86e43dbbb88c9f67255
Ivan <ivan.artemov.04@gmail.com>
2025-03-13 15:06:52 +0300

    change after review

--- backend/main_app/settings.py
+++ backend/main_app/settings.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 from environs import Env
@@ -72,12 +71,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
